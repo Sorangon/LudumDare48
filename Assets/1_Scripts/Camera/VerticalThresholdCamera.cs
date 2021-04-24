3b6016337cb1408081557964e1100736245a51b2
JulienDelaunay <julien.delaunay034@gmail.com>
2021-04-24 17:09:57 +0200

    Basic vertical camera system

--- Assets/1_Scripts/Camera/VerticalThresholdCamera.cs
+++ Assets/1_Scripts/Camera/VerticalThresholdCamera.cs
@@ -3,23 +3,51 @@ using UnityEngine;
 public class VerticalThresholdCamera : MonoBehaviour {
     #region Settings
     [Header("Settings")]
-    public float damping = 1f;
-    public float verticalOffset = 1f;
+    [Min(0f)]public float damping = 1f;
+    public float heightThreshold = -1f;
     public float maxDistance = 5f;
 
     [Header("References")]
     public Transform followTarget = null;
     #endregion
 
+    #region Currents
+    private float targetHeight = 800f;
+    #endregion
+
     #region Callbacks
     private void LateUpdate() {
+        if (ReferenceEquals(followTarget, null)) return;
+
+        if (followTarget.transform.position.y <= targetHeight + heightThreshold) {
+            targetHeight = followTarget.transform.position.y - heightThreshold;
+        }
+
+        Vector3 targetPos = new Vector3(transform.position.x, targetHeight, transform.position.z);
+        if (damping > 0) {
+            transform.position = Vector3.Lerp(transform.position, targetPos, Time.deltaTime / damping);
+        } else {
+            transform.position = targetPos;
+        }
     }
     #endregion
 
     #region Debug
     private void OnDrawGizmosSelected() {
+        void DrawVerticalLine(float height) {
+            Gizmos.DrawLine(new Vector2(-500f, height), new Vector2(500f, height));
+        }
+
+        Gizmos.color = Color.green;
+        DrawVerticalLine(transform.position.y + heightThreshold);
+
         if (Application.isPlaying) {
-            //Gizmos.DrawLine(-Vector3.right * )
+            Gizmos.color = Color.yellow;
+            DrawVerticalLine(targetHeight + heightThreshold);
+
+            if (followTarget != null) {
+                Gizmos.DrawSphere(followTarget.position, 0.2f);
+            }
         }
     }
     #endregion
