21e66def33a5c25a495a605128b26abfb863c94a
JulienDelaunay <julien.delaunay034@gmail.com>
2021-04-24 15:55:25 +0200

    Created destroyable tiles

---
+++ Assets/1_Scripts/GPE/Health.cs
@@ -0,0 +1,94 @@
+﻿using UnityEngine;
+using NaughtyAttributes;
+using UnityEngine.Events;
+
+/// <summary>
+/// Base health system for game objects
+/// </summary>
+public class Health : MonoBehaviour {
+    #region Settings
+    [SerializeField, Min(0)] private int maxHealth = 100;
+    public bool invicible = false;
+
+    [Header("Death")]
+    [SerializeField] private DeathBehaviour deathBehaviour = null;
+    [SerializeField] private Object deathBehaviourParameter = null;
+
+    [Foldout("Events")]
+    [SerializeField] private UnityEvent onHeal = new UnityEvent();
+    [Foldout("Events")]
+    [SerializeField] private UnityEvent onTakeDamages = new UnityEvent();
+    [Foldout("Events")]
+    [SerializeField] private UnityEvent onAddMaxHealth = new UnityEvent();
+    [Foldout("Events")]
+    [SerializeField] private UnityEvent onDie = new UnityEvent();
+
+    #endregion
+
+    #region Currents
+    [ShowNonSerializedField] private int currentHealth = 100;
+    #endregion
+
+    #region Properties
+    public int MaxHealth => maxHealth;
+    public int CurrentHealth => currentHealth;
+    public float CurrentHealthRatio => (float)currentHealth / (float)maxHealth;
+    #endregion
+
+    #region Callbacks
+    private void Start() {
+        if (currentHealth > maxHealth) {
+            currentHealth = maxHealth;
+        }
+    }
+    #endregion
+
+    #region Update Health
+    public void Heal(int amount) {
+        if (amount < 0) {
+            return;
+        }
+
+        currentHealth += amount;
+        if (currentHealth > maxHealth) {
+            currentHealth = maxHealth;
+        }
+
+        onHeal?.Invoke();
+    }
+
+    public void InflictDamages(int amount) {
+        if (amount < 0 || invicible) {
+            return;
+        }
+
+        currentHealth -= amount;
+        if (currentHealth <= 0) {
+            Die();
+        }
+
+        onTakeDamages?.Invoke();
+    }
+
+    public void AddMaxHealth(int amount, float healthPercentageToAdd = 0f) {
+        maxHealth += amount;
+
+        if (healthPercentageToAdd > 0) {
+            currentHealth += Mathf.CeilToInt(amount * healthPercentageToAdd);
+        }
+
+        onAddMaxHealth?.Invoke();
+    }
+    #endregion
+
+    #region Die
+    public void Die() {
+        onDie?.Invoke();
+        if (deathBehaviour != null) {
+            deathBehaviour.Execute(this, deathBehaviourParameter);
+        } else {
+            Destroy(gameObject);
+        }
+    }
+    #endregion
+}
--- Assets/1_Scripts/Weapons/Projectiles/Projectile.cs
+++ Assets/1_Scripts/Weapons/Projectiles/Projectile.cs
@@ -2,9 +2,17 @@ using UnityEngine;
 
 [SelectionBase]
 public class Projectile : MonoBehaviour {
+    #region Settings
+    [Min(0)] public int damages = 10;
+    #endregion
+
     #region Physic Callbacks
     private void OnTriggerEnter2D(Collider2D collision) {
         //Check collision with damageable object
+        GameObject rootGo = collision.attachedRigidbody != null ? collision.attachedRigidbody.gameObject : collision.gameObject;
+        if (rootGo.TryGetComponent(out Health health)) {
+            health.InflictDamages(damages);
+        }
 
         Destroy(gameObject);
     }
