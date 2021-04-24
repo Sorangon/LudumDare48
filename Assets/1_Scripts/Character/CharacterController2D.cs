793b55130d806f106cf6f38f5340f03e00344d90
JulienDelaunay <julien.delaunay034@gmail.com>
2021-04-24 12:06:03 +0200

    WIP character controller

---
+++ Assets/1_Scripts/Character/CharacterController2D.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//using 
+
+[RequireComponent(typeof(CharacterPhysics))]
+public class CharacterController2D : MonoBehaviour {
+    #region Settings
+    public CharacterPhysics characterPhysics = null;
+    #endregion
+}
---
+++ Assets/1_Scripts/Character/CharacterInput.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+[RequireComponent(typeof(CharacterController2D))]
+public class CharacterInput : MonoBehaviour {
+    public CharacterController2D controller = null;
+}
