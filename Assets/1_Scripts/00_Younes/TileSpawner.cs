2ecf84dcdae94c17d54baa798534c6ed9785b2ab
Exyde <54008677+Exyde@users.noreply.github.com>
2021-04-24 11:39:39 +0200

    Create folder and script structure for procedural Generation

---
+++ Assets/1_Scripts/00_Younes/TileSpawner.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TileSpawner : MonoBehaviour
+{
+    [SerializeField] GameObject tilePrefab;
+
+    void Start()
+    {
+        ²
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
