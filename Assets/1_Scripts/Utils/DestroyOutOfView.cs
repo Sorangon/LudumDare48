8bb143bbb7ef6005386c128fb3f46f4923327052
JulienDelaunay <julien.delaunay034@gmail.com>
2021-04-24 18:07:46 +0200

    Projectiles are destroyed when those ones are out of view

---
+++ Assets/1_Scripts/Utils/DestroyOutOfView.cs
@@ -0,0 +1,25 @@
+using CoreDev.Utility;
+using UnityEngine;
+
+public class DestroyOutOfView : MonoBehaviour {
+    public float toleranceDistance = 0f;
+
+    void LateUpdate() {
+        Camera cam = MainCameraBuffer.Get();
+        Vector2 screenPos = cam.WorldToScreenPoint(transform.position);
+        float tolerancePixelDistance = cam.WorldToScreenPoint(cam.transform.position + Vector3.right * toleranceDistance).x;
+        tolerancePixelDistance -= cam.pixelRect.width * 0.5f;
+        if (RectContains(screenPos, cam.pixelRect, tolerancePixelDistance)){
+            Destroy(gameObject);
+        }
+    }
+
+    private bool RectContains(Vector2 screenPos, Rect rect, float tolerancePixelDistance) {
+        if (screenPos.x + tolerancePixelDistance < rect.x || screenPos.y + tolerancePixelDistance < rect.y ||
+            screenPos.x - tolerancePixelDistance > rect.x + rect.width || screenPos.y - tolerancePixelDistance < rect.y + rect.height) {
+            return false;
+        } else {
+            return true;
+        }
+    }
+}
