2cf72e0011cedeaf4ae5b072991956fce6facb38
JulienDelaunay <julien.delaunay034@gmail.com>
2021-04-24 18:21:52 +0200

    Fix destroy out of view

--- Assets/1_Scripts/Utils/DestroyOutOfView.cs
+++ Assets/1_Scripts/Utils/DestroyOutOfView.cs
@@ -2,24 +2,22 @@ using CoreDev.Utility;
 using UnityEngine;
 
 public class DestroyOutOfView : MonoBehaviour {
-    public float toleranceDistance = 0f;
-
     void LateUpdate() {
         Camera cam = MainCameraBuffer.Get();
         Vector2 screenPos = cam.WorldToScreenPoint(transform.position);
-        float tolerancePixelDistance = cam.WorldToScreenPoint(cam.transform.position + Vector3.right * toleranceDistance).x;
-        tolerancePixelDistance -= cam.pixelRect.width * 0.5f;
-        if (RectContains(screenPos, cam.pixelRect, tolerancePixelDistance)){
+        //float tolerancePixelDistance = cam.WorldToScreenPoint(cam.transform.position + Vector3.right * toleranceDistance).x;
+        //tolerancePixelDistance -= cam.pixelRect.width * 0.5f;
+        if (!cam.pixelRect.Contains(screenPos)){
             Destroy(gameObject);
         }
     }
 
-    private bool RectContains(Vector2 screenPos, Rect rect, float tolerancePixelDistance) {
-        if (screenPos.x + tolerancePixelDistance < rect.x || screenPos.y + tolerancePixelDistance < rect.y ||
-            screenPos.x - tolerancePixelDistance > rect.x + rect.width || screenPos.y - tolerancePixelDistance < rect.y + rect.height) {
-            return false;
-        } else {
-            return true;
-        }
-    }
+    //private bool RectContains(Vector2 screenPos, Rect rect, float tolerancePixelDistance) {
+    //    if (screenPos.x + tolerancePixelDistance < rect.x || screenPos.y + tolerancePixelDistance < rect.y ||
+    //        screenPos.x - tolerancePixelDistance > rect.x + rect.width || screenPos.y - tolerancePixelDistance < rect.y + rect.height) {
+    //        return false;
+    //    } else {
+    //        return true;
+    //    }
+    //}
 }
