616d7f318a143d220da9f32ebd680442ff7c044e
JulienDelaunay <julien.delaunay034@gmail.com>
2021-04-25 17:07:53 +0200

    Created feedback bullet impact

--- Assets/1_Scripts/Weapons/Projectiles/Projectile.cs
+++ Assets/1_Scripts/Weapons/Projectiles/Projectile.cs
@@ -1,9 +1,14 @@
+using NaughtyAttributes;
 using UnityEngine;
+using UnityEngine.Events;
 
 [SelectionBase]
 public class Projectile : MonoBehaviour {
     #region Settings
     [Min(0)] public int damages = 10;
+
+    [Foldout("Events")]
+    public UnityEvent onImpact = new UnityEvent();
     #endregion
 
     #region Physic Callbacks
@@ -13,6 +18,7 @@ public class Projectile : MonoBehaviour {
             healthSystem.InflictDamages(damages);
         }
 
+        onImpact?.Invoke();
         Destroy(gameObject);
     }
     #endregion
