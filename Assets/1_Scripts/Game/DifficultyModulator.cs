b8c9714a0a9718ca795ce15e16848451b960fcce
JulienDelaunay <julien.delaunay034@gmail.com>
2021-04-25 10:53:04 +0200

    Increment difficulty across the time

--- Assets/1_Scripts/Game/DifficultyModulator.cs
+++ Assets/1_Scripts/Game/DifficultyModulator.cs
@@ -1,19 +1,33 @@
 using NaughtyAttributes;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 [CreateAssetMenu(fileName = "DM_", menuName = "Game/Difficulty Modulator")]
 public class DifficultyModulator : ScriptableObject {
     #region Settings
+    [Range(0, 1f)] public float percentageIncrementPerSeconds = 0.02f;
     [MinMaxSlider(0f, 1f)] public Vector2 minDifficultyAmount = new Vector2(0f, 1f);
     #endregion
 
     #region Current
-    private float difficultyAmount = 0f;
+    [ShowNonSerializedField] private float difficultyAmount = 0f;
     #endregion
 
     #region Properties
     public float DifficultyAmount => difficultyAmount;
+    #endregion
+
+    #region Difficulty
+    public void Update() {
+        if(difficultyAmount < 1f) {
+            difficultyAmount += Time.deltaTime * percentageIncrementPerSeconds;
+            if(difficultyAmount > 1f) {
+                difficultyAmount = 1f;
+            }
+        }
+    }
+
+    public void Reinitialize() {
+        difficultyAmount = 0f;
+    }
     #endregion
 }
