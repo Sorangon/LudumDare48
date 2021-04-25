5054ba0f9d1492f094bc2f26abefdf3506e39af7
Exyde <54008677+Exyde@users.noreply.github.com>
2021-04-25 12:48:45 +0200

    Spawning Dynamite and Ennemies : D

--- Assets/1_Scripts/00_Younes/TileSpawner.cs
+++ Assets/1_Scripts/00_Younes/TileSpawner.cs
@@ -21,6 +21,7 @@ public class TileSpawner : MonoBehaviour
     [Range(0, 100)] public int fillPercent;
     [Range(0, 80)] public int wallPercent;
     [Range(0, 20)] public int enemyPercent;
+    [Range(0, 20)] public int dynamitePercent;
 
     int globalHeight;
 
@@ -41,6 +42,9 @@ public class TileSpawner : MonoBehaviour
             for (int i = 0; i < grid2D.width; i++)
             {
                 int wallRng = Random.Range(0, 100);
+                int enemyRng = Random.Range(0, 100);
+                int dynamiteRng = Random.Range(0, 100);
+
 
                 if (!fillLines)
                 {
@@ -48,7 +52,15 @@ public class TileSpawner : MonoBehaviour
 
                     if (fillRng < fillPercent)
                     {
-                        if (wallRng > wallPercent)
+                        if (dynamiteRng < dynamitePercent)
+						{
+                            CreateTilePrefab(dynamitePrefab, i, globalHeight + chunkHeight + n, holder.transform);
+                        }
+                        else if (enemyRng < enemyPercent)
+						{
+                            CreateTilePrefab(enemyPrefab, i, globalHeight + chunkHeight + n, holder.transform);
+                        }
+                        else if (wallRng > wallPercent)
                         {
                             CreateTilePrefab(tilePrefab, i, globalHeight + chunkHeight + n, holder.transform);
                         } else
@@ -59,7 +71,15 @@ public class TileSpawner : MonoBehaviour
                 }
                 else
                 {
-                    if (wallRng > wallPercent)
+                    if (dynamiteRng < dynamitePercent)
+                    {
+                        CreateTilePrefab(dynamitePrefab, i, globalHeight + chunkHeight + n, holder.transform);
+                    }
+                    else if (enemyRng < enemyPercent)
+                    {
+                        CreateTilePrefab(enemyPrefab, i, globalHeight + chunkHeight + n, holder.transform);
+                    }
+                    else if (wallRng > wallPercent)
                     {
                         CreateTilePrefab(tilePrefab, i, globalHeight + chunkHeight + n, holder.transform);
                     }
@@ -93,20 +113,15 @@ public class TileSpawner : MonoBehaviour
 
     void UpdateDifficulty()
     {
-        //Difficulty update
-        //Fill pct++
-        //Wall pct++
-        //Chunk height ++
-
         float d = dm.DifficultyAmount; //Between 0 or 1
 
-        //Lower max chunk per level & Lower space Btw chunks
         if (d < .1)
         {
             chunkHeight = 2;
             fillPercent = 50;
             wallPercent = 0;
             enemyPercent = 0;
+            dynamitePercent = 0;
         }
         else if (d < .2)
         {
@@ -114,6 +129,7 @@ public class TileSpawner : MonoBehaviour
             fillPercent = 55;
             wallPercent = 2;
             enemyPercent = 2;
+            dynamitePercent = 1;
         }
         else if (d < .3)
         {
@@ -121,6 +137,8 @@ public class TileSpawner : MonoBehaviour
             fillPercent = 60;
             wallPercent = 5;
             enemyPercent = 2;
+            dynamitePercent = 2;
+
         }
         else if (d < .4)
         {
@@ -128,6 +146,7 @@ public class TileSpawner : MonoBehaviour
             fillPercent = 65;
             wallPercent = 8;
             enemyPercent = 3;
+            dynamitePercent = 3;
         }
         else if (d < .5)
         {
@@ -135,6 +154,7 @@ public class TileSpawner : MonoBehaviour
             fillPercent = 70;
             wallPercent = 10;
             enemyPercent = 4;
+            dynamitePercent = 3;
 
         }
         else if (d < .6)
@@ -143,6 +163,7 @@ public class TileSpawner : MonoBehaviour
             fillPercent = 75;
             wallPercent = 10;
             enemyPercent = 5;
+            dynamitePercent = 3;
         }
         else if (d < .7)
         {
@@ -150,6 +171,8 @@ public class TileSpawner : MonoBehaviour
             fillPercent = 75;
             wallPercent = 12;
             enemyPercent = 5;
+            dynamitePercent = 3;
+
         }
         else if (d < .8)
         {
@@ -157,6 +180,8 @@ public class TileSpawner : MonoBehaviour
             fillPercent = 80;
             wallPercent = 14;
             enemyPercent = 5;
+            dynamitePercent = 4;
+
         }
 
         else if (d < .9)
@@ -165,6 +190,8 @@ public class TileSpawner : MonoBehaviour
             fillPercent = 85;
             wallPercent = 17;
             enemyPercent = 6;
+            dynamitePercent = 5;
+
         }
         else
         {
@@ -172,6 +199,8 @@ public class TileSpawner : MonoBehaviour
             fillPercent = 90;
             wallPercent = 20;
             enemyPercent = 6;
+            dynamitePercent = 7;
+
         }
     }
 }
